fed9045a0780463037e98ab2d1407a5204549014
Crowdbotics <team@crowdbotics.com>
2022-08-21 13:22:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "syllabus_planner_36853",
+  url: "https://syllabus_planner_36853.botics.co",
+  api: "https://syllabus_planner_36853.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
